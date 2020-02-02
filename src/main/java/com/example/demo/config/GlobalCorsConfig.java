e3b0c8a26ec08a3e2709cf83a2bbd091d5fe7550
buer <18319748706@163.com>
2020-02-03 00:26:49 +0800

    :star:前后token

--- src/main/java/com/example/demo/config/GlobalCorsConfig.java
+++ src/main/java/com/example/demo/config/GlobalCorsConfig.java
@@ -18,7 +18,7 @@ public class GlobalCorsConfig  implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD"); // or *
+        response.setHeader("Access-Control-Allow-Methods", "*"); // or *
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*"); // or *
         HttpServletRequest request = (HttpServletRequest) req;
