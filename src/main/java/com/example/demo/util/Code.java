dfd84e10f4eaab000f5f318754d3cfb09eff5ebb
buerbl <18319748706@163.com>
2020-11-07 18:14:16 +0800

    :star:add

--- src/main/java/com/example/demo/util/Code.java
+++ src/main/java/com/example/demo/util/Code.java
@@ -1,18 +1,17 @@
 package com.example.demo.util;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+        import lombok.AllArgsConstructor;
+        import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum Code {
-    SUCCESS(200,"成功"),
+    SUCCESS(200, "成功"),
     USERNAMEERROR(501, "用户名错误"),
     PASSWORDERROR(502, "密码错误"),
     NORIGHT(503, "没有授权"),
     NOSIGNIN(504, "没有登录"),
-    ERROR(500, "报错了")
-    ;
+    ERROR(500, "报错了");
     private int code;
     private String msg;
 
