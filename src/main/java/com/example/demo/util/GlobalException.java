81c3256a96b1e42c1f024aa417393eb24f214a15
buerbl <18319748706@163.com>
2022-10-06 10:52:41 +0800

    add
    add

--- src/main/java/com/example/demo/util/GlobalException.java
+++ src/main/java/com/example/demo/util/GlobalException.java
@@ -54,6 +54,7 @@ public class GlobalException {
         return result;
     }
 
+    
     @ExceptionHandler({Exception.class})
     public Result exception(Exception e) {
         Result result = new Result();
